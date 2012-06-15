c3efec61e1c4b9cbf7f6d871c0d92b4057897e0b
Hu Yuhuang <duguyue100@gmail.com>
2012-06-16 00:14:38 +0800

    t_1_1

---
+++ Tutorial_01/HelloWorld.java
@@ -0,0 +1,10 @@
+// Author: Hu Yuhuang
+// E-mail: duguyue100@gmail.com
+// Tutorial 01 01
+
+public class HelloWorld {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
